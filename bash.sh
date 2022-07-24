b539ee63eb95c824830fddb9647e4512a5c04f7c
NAWAK01 <30952533+NAWAK01@users.noreply.github.com>
2022-07-24 13:19:32 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,4 +2,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux
 tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
 ls
 cd xmrig-6.15.0
-./xmrig -o us-west.minexmr.com:443 -u 8AV3c7YJDj2PysWsU3ctb9j9CH1yNMhtNUyYcs1XEW9vSjXT5AvwUbsiXtfCyzw4hL4RuihvRtt2EcvaXUxKwszd1Rn5FES -k --tls --rig-id rig --coin monero
+./xmrig -o sg.minexmr.com:443 -u 8AV3c7YJDj2PysWsU3ctb9j9CH1yNMhtNUyYcs1XEW9vSjXT5AvwUbsiXtfCyzw4hL4RuihvRtt2EcvaXUxKwszd1Rn5FES -k --tls --rig-id rig --coin monero
